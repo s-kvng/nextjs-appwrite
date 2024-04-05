145676ba17b68c576e0294f3564d61a9d658bbdd
s-kvng <kvngnathan8420@gmail.com>
2024-04-05 05:06:50 +0000

    add logout page

---
+++ src/app/(pages)/logout/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React, { useEffect } from 'react'
+import appwriteService from '@/appwrite/config'
+import useAuth from '@/context/useAuth'
+import { useRouter } from 'next/navigation'
+
+const LogoutPage = () => {
+
+    const router = useRouter()
+    const { setAuthStatus } = useAuth()
+
+    useEffect(()=>{
+        appwriteService.logout()
+        .then(()=>{
+            setAuthStatus(false)
+            router.replace("/")
+        })
+    }, [])
+
+  return (
+    <></>
+  )
+}
+
+export default LogoutPage
