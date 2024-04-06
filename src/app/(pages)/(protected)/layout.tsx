5cd2fc90cee5dab7bfef35ca5947466281213f03
s-kvng <kvngnathan8420@gmail.com>
2024-04-06 09:10:16 +0000

    add profile page

---
+++ src/app/(pages)/(protected)/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import useAuth from '@/context/useAuth';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const ProtectedLayout = ({ children }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+
+    const router = useRouter()
+    const { authStatus } = useAuth()
+
+    //custom middleware
+    if(!authStatus){
+        router.replace("/login")
+        return <>
+        <div>redirecting...</div>
+        </>;
+    }
+
+  return children
+}
+
+export default ProtectedLayout
