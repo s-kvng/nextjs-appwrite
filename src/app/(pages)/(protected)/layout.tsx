9c6a17de3ab9ebe3b17ad7ab95dcf2e8548189b3
s-kvng <kvngnathan8420@gmail.com>
2024-04-07 01:56:04 +0000

    update profile layout

--- src/app/(pages)/(protected)/layout.tsx
+++ src/app/(pages)/(protected)/layout.tsx
@@ -2,26 +2,39 @@
 
 import useAuth from '@/context/useAuth';
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProtectedLayout = ({ children }: Readonly<{
     children: React.ReactNode;
   }>) => {
 
     const router = useRouter()
-    const { authStatus } = useAuth()
+// Fetch initial auth status from context
+const { authStatus } = useAuth();
+
+// Store auth status in state for conditional rendering
+const [isAuthenticated, setIsAuthenticated] = useState(authStatus);
 
     //custom middleware
-    if(!authStatus){
-        router.replace("/login")
-        return <></>;
-    }
+    useEffect(() => {
+      // Perform redirection if user is not authenticated
+      if (!isAuthenticated) {
+        router.replace('/login');
+      }
+    }, [isAuthenticated, authStatus]); // Run the effect when authStatus changes
 
-  return (
-    <>
-    {children}
-    </>
-  )
-}
+
+    return (
+      <div>
+        {isAuthenticated ? (
+          <>
+            {children}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 
 export default ProtectedLayout
