c1914b43a0c3e0ea501282dfbc929c5da0539513
s-kvng <kvngnathan8420@gmail.com>
2024-04-07 00:28:18 +0000

    update navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useAuth from "@/context/useAuth";
 import Link from "next/link";
-import React from "react";
+import React, { useState} from "react";
 import Logo from "./Logo";
 
 const menuItems = [
@@ -20,6 +20,13 @@ const menuItems = [
 ];
 
 export default function Header() {
+
+    const [isOpen, setIsOpen] = useState(false); // State to track menu visibility
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
     const { authStatus } = useAuth();
     return (
         <div className="relative w-full bg-white py-2">
@@ -57,6 +64,54 @@ export default function Header() {
                         {authStatus ? "Logout" : "Log In"}
                     </Link>
                 </div>
+
+                {/* Mobile view */}
+            <div className="-mr-2 flex lg:hidden">
+                <button
+                    type="button"
+                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                    aria-controls="mobile-menu"
+                    aria-expanded={isOpen} // Update aria-expanded based on isOpen state
+                    onClick={toggleMenu}
+                >
+                    Menu
+                </button>
+                <div
+                    className={` z-10 ml-3 space-y-1 sm:ml-6 absolute top-0 right-0 bg-white shadow-xl overflow-hidden h-screen w-full transition transform duration-300 ease-in-out ${
+                    isOpen ? '' : 'translate-x-full' // Slide menu in from right
+                    }`}
+                    id="mobile-menu"
+                    aria-hidden={!isOpen} // Update aria-hidden based on isOpen state
+                >
+                    {menuItems.map((item) => (
+                    <Link
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                        onClick={toggleMenu} // Close menu on item click (optional)
+                    >
+                        {item.name}
+                    </Link>
+                    ))}
+
+                <div className="lg:hidden space-x-2 flex flex-col">
+                    <Link
+                        onClick={toggleMenu}
+                        href={authStatus ? "/profile" : "/signup"}
+                        className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-primary hover:bg-primary/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                    >
+                        {authStatus ? "Profile" : "Sign up"}
+                    </Link>
+                    <Link
+                        onClick={toggleMenu}
+                        href={authStatus ? "/logout" : "/login"}
+                        className=" w-1/5 flex justify-center items-center rounded-md border border-primary px-3 py-2 text-sm font-semibold text-primary shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                    >
+                        {authStatus ? "Logout" : "Log In"}
+                    </Link>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
     );
